1d0e2a06d5cf18a95e663ace9af6638b92e9da52
mufqiuwais <50223787+mufqiuwais@users.noreply.github.com>
2019-06-24 20:26:51 +0700

    final

--- src/Watashi.java
+++ src/Watashi.java
@@ -14,12 +14,12 @@ public class Watashi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Watashi (The Tatami Galaxy)");
+        JFrame frame = new JFrame("Watashi (The Tatami Galaxy) || Mufqi Uwais Nastiar Salim (181524017)");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         WatashiJPanel watashiJPanel = new WatashiJPanel();
         frame.add(watashiJPanel);
-        frame.setSize(800,600);
+        frame.setSize(820,600);
         frame.setVisible(true);
     }
     
